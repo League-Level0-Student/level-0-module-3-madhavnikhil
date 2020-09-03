8433e353d21c54a711d6d6baec013d6f8d9dc6a3
root <root@f853e9e2daff>
2020-09-03 23:54:29 +0000

    BananaQuiz

--- src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
+++ src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
@@ -1,26 +1,40 @@
 
 package _03_modulo._4_fizz_buzz;
 
+import javax.swing.JOptionPane;
 
 /**
-* Fizz Buzz
-* 
-* In this project, we're going to build FizzBuzz. It's a children's game where
-* you count from 1 to 20. Easy, right? Here's the catch: 
-* 
-* Instead of saying a number that is divisible by 3, say "Fizz".
-* And instead of saying a number that is divisible by 5, say "Buzz".
-* For numbers divisible by both 3 and 5, say "FizzBuzz".
-* 
-* Print your results to the console.
-* 
-* If your code is correct, the output will be:
-*  
-* 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz
-* 
-**/
-
+ * Fizz Buzz
+ * 
+ * In this project, we're going to build FizzBuzz. It's a children's game where
+ * you count from 1 to 20. Easy, right? Here's the catch:
+ * 
+ * 
+ * Instead of saying a number that is divisible by 3, say "Fizz". And instead of
+ * saying a number that is divisible by 5, say "Buzz". For numbers divisible by
+ * both 3 and 5, say "FizzBuzz".
+ * 
+ * Print your results to the console.
+ * 
+ * If your code is correct, the output will be:
+ * 
+ * 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz
+ * 
+ **/
 public class FizzBuzz {
+	public static void main(String[] args) {
+		for (int Vernon = 1; Vernon < 21; Vernon++) {
+			if (Vernon % 3 == 0 && Vernon % 5 == 0)
+			System.out.println("fizzbuzz");
+			else if (Vernon % 3 == 0) {
+				System.out.println("buzz");
+			} else if (Vernon % 5 == 0){
+				System.out.println("fizz");
+			}
+			else {
+				System.out.println(Vernon);
+			}
+		}
 
+	}
 }
-
--- src/_99_extra/BananaQuiz.java
+++ src/_99_extra/BananaQuiz.java
@@ -9,15 +9,24 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String Bellatrix = JOptionPane.showInputDialog("Do you like bananas?");
 		//2. if they say no, 
 			//tell them they are crazy 
+		if (Bellatrix.equalsIgnoreCase("No")) {
+			JOptionPane.showMessageDialog(null, "You are crazy.");
+		}
 			//and end quiz
 		//3. if they say yes
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
-
+	
+	  else if (Bellatrix.equalsIgnoreCase("Yes")) {
+	     String Umbridge = JOptionPane.showInputDialog("What is your favorite hobby?");
+	     JOptionPane.showMessageDialog(null, Umbridge + " is better with bananas.");
+	     
 		//4. OPTIONAL: if they say something other than "yes?" or "no?"
 		//	show a pop up that says "You are bananas!?"
+	  }
 	
 	}
 
