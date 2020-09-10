d6cf77e4ab1aa489faf0bc369af050aefc29ba35
root <root@ca92a6505a56>
2020-09-10 23:45:13 +0000

    dizzy robot

--- src/_99_extra/BirthdayReminder.java
+++ src/_99_extra/BirthdayReminder.java
@@ -8,18 +8,27 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String Vernon = "September 14";
+		String Petunia = "September 3";
+		String Dudley = "July 23";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+    String Harry = JOptionPane.showInputDialog("Which birthday do you want to see?");
 		// 3. Print out what the user typed
 		
 		// 4. if user asked for "mom"
 			//print mom's birthday
+    		if (Harry.equalsIgnoreCase("Vernon")) {
+		       JOptionPane.showMessageDialog(null, Vernon);
+			}
 		// 5. if user asked for "dad"
 			// print dad's birthday
+    		if (Harry.equalsIgnoreCase("Petunia")) {
+    			JOptionPane.showMessageDialog(null, Petunia);
+    		}
+    		if (Harry.equalsIgnoreCase("Dudley"))  {
+    			 JOptionPane.showMessageDialog(null, Dudley);
+	}
 		// 6. if user asked for your name
 			// print myBirthday
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
--- src/_99_extra/DizzyRobot.java
+++ src/_99_extra/DizzyRobot.java
@@ -1,16 +1,20 @@
 
 package _99_extra;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
 	static Robot robot = new Robot();
 	public static void main(String[] args) {
 		
-		// 1. Use the dance method below to make the robot spin.		
+		// 1. Use the dance method below to make the robot spin.
+		
 		// 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
-	
-
+	String Heronarry = JOptionPane.showInputDialog("How many times do you want the robot to spin?");
+int Haronie = Integer.parseInt(Heronarry);
+dance(Haronie);
 	}
 
 	/*****************   Use this method, DON'T CHANGE THE CODE BELOW  **************/
