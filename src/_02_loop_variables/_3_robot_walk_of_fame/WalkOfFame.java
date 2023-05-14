3be18f6761304eee1eea82e56e1100623a54829d
root <root@a7c80a986057>
2023-05-14 19:42:26 +0000

    shinyobjects

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -1,6 +1,8 @@
 
 package _02_loop_variables._3_robot_walk_of_fame;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
@@ -10,17 +12,33 @@ public class WalkOfFame {
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
 
-		// 2. Make the robot draw a star shape. Hint: angle=144.
+		rob.setX(100);
+		rob.penDown();
+		rob.hide();
+		rob.setSpeed(10);
+		rob.setPenColor(Color.BLACK);
+		
+		 for (int j = 0; j < 11; j++) {
+	
 
-		// 3. Set the length of each line in the star to 30.
-
+		for (int i = 0; i < 5; i++) {
+			
+			// 2. Make the robot draw a star shape. Hint: angle=144.
+			rob.turn(144);
+			// 3. Set the length of each line in the star to 30.
+			rob.move(30);
+						
+		}
+		rob.move(30);
+		}
+	}
+}
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
-		 * 
+   
 		 * Hint: The distance between stars is 50.
+	
 		 */
+	
 
-	}
-
-}
